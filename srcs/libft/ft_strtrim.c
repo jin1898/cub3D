944530d8fca05e042051ac1d3a666927c96d875a
asteadygoer <asteadygoer@naver.com>
2023-09-26 21:48:18 +0900

    changed : strtrim

--- srcs/libft/ft_strtrim.c
+++ srcs/libft/ft_strtrim.c
@@ -6,35 +6,78 @@
 /*   By: sunwoo-jin <sunwoo-jin@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 18:42:17 by jeekpark          #+#    #+#             */
-/*   Updated: 2023/09/22 10:30:18 by sunwoo-jin       ###   ########.fr       */
+/*   Updated: 2023/09/26 21:42:36 by sunwoo-jin       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s1, char const *set)
+static int	is_it_set(const char *location, const char *set)
 {
-	size_t	s;
-	size_t	e;
-	size_t	i;
-	char	*res;
+	size_t	set_strlen;
+	size_t	index;
 
-	s = 0;
-	e = (size_t) ft_strlen(s1);
-	while (s1[s] && ft_strchr(set, s1[s]) != 0)
-		s++;
-	while (e > 0 && ft_strchr(set, s1[e]) != 0)
-		e--;
-	if (s > e || (s == 0 && e == 0))
-		i = 1;
-	else
-		i = e - s + 2;
-	res = (char *) malloc(sizeof(char) * i);
-	if (res == (void *) 0)
-		return ((void *) 0);
+	index = 0;
+	set_strlen = ft_strlen(set);
+	while (index < set_strlen)
+	{
+		if (set[index] == *location)
+			return (1);
+		index++;
+	}
+	return (0);
+}
+
+static char	*mal(const char *start, const char *end)
+{
+	int		i;
+	int		string_strlen;
+	char	*box;
+
+	string_strlen = end - start;
 	i = 0;
-	while (s <= e)
-		res[i++] = s1[s++];
-	res[i] = '\0';
-	return (res);
+	box = malloc(sizeof(char) * (string_strlen + 2));
+	if (box == NULL)
+		return (NULL);
+	box[string_strlen + 1] = 0;
+	while (i <= string_strlen)
+	{
+		box[i] = start[i];
+		i++;
+	}
+	return (box);
+}
+
+static char	*no_mal(void)
+{
+	char	*box;
+
+	box = malloc(sizeof(char));
+	if (box == NULL)
+		return (NULL);
+	box[0] = 0;
+	return (box);
+}
+
+char	*ft_strtrim(const char *s1, const char *set)
+{
+	size_t		i;
+	const char	*start;
+	const char	*end;
+
+	i = 0;
+	start = NULL;
+	end = NULL;
+	while (s1[i] != '\0')
+	{
+		if (is_it_set(&s1[i], set) == 0 && start == NULL)
+			start = &s1[i];
+		if (is_it_set(&s1[i], set) == 0)
+			end = &s1[i];
+		i++;
+	}
+	if (start && end)
+		return (mal(start, end));
+	else
+		return (no_mal());
 }
